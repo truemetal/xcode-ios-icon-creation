f9291722f9f2aaf6cb81c9a7e198441a570b4776
Bogdan <true.metal.of.steel@gmail.com>
2014-11-15 09:41:44 +0200

    add support for specifying icon path as command-line argument

--- README.md
+++ README.md
@@ -3,9 +3,10 @@ xcode-ios-icon-creation
 
 This tiny script can save you a great deal of time creating icons for your iOS app. Just run the script in your console and it will automatically create icons in all resolutions that you can use in your Xcode iOS project. 
 
+You can place the script into your `/usr/bin` to run it like `make_icons.swift`; otherwise specify full/relative path to script e.g. `./make_icons.swift`. You can also freely remove `.swift` extension. 
+
 Usage: 
 -----
 
-1. Place your icon in big resolution next to the script. Name it icon.png
-2. In Terminal cd to folder location 
-3. Run `./make_icons.swift` 
+`make_icons.swift <icon_name(s)>`
+where icon_name is relative or full path to the file
--- make_icons.swift
+++ make_icons.swift
@@ -1,10 +1,14 @@
 #!/usr/bin/env xcrun swift
 import Foundation
 
-let iconCreatorFolder = NSFileManager.defaultManager().currentDirectoryPath
-let sourceIcon = iconCreatorFolder.stringByAppendingPathComponent("icon.png")
-let outputDir = iconCreatorFolder.stringByAppendingPathComponent("output")
+let currentPath = NSFileManager.defaultManager().currentDirectoryPath
+let defaultSourceIconPath = currentPath.stringByAppendingPathComponent("icon.png")
 let iconResolutions = [29, 40, 58, 76, 80, 87, 120, 152, 180]
+
+func outputDirPathForIconPath(iconPath: String) -> String
+{
+    return iconPath.stringByDeletingLastPathComponent.stringByAppendingPathComponent("\(iconPath.lastPathComponent.stringByDeletingPathExtension)_output")
+}
 
 func scaleIcon(iconPath: String, resolution: Int)
 {
@@ -14,23 +18,80 @@ func scaleIcon(iconPath: String, resolution: Int)
     task.launch()
 }
 
-if NSFileManager.defaultManager().fileExistsAtPath(iconCreatorFolder) == false
+func createIconsForFilePaths(filePaths : Array<String>)
 {
-    println("Icon creation folder does not exist!")
+    var failedInputsCheck = false
+    for filePath in filePaths
+    {
+        if NSFileManager.defaultManager().fileExistsAtPath(filePath) == false {
+            failedInputsCheck = true
+            println("Issue: file does not exist \(filePath)")
+        }
+        
+        if NSFileManager.defaultManager().fileExistsAtPath(outputDirPathForIconPath(filePath)) {
+            failedInputsCheck = true
+            println("Issue: this directory exists and will be re-written \(outputDirPathForIconPath(filePath))")
+        }
+    }
+    
+    if failedInputsCheck { return }
+    
+    for iconPath in filePaths
+    {
+        println("Processing file: \(iconPath)")
+        
+        let outputDir = outputDirPathForIconPath(iconPath)
+        NSFileManager.defaultManager().createDirectoryAtPath(outputDir, withIntermediateDirectories: false, attributes: nil, error: nil)
+        
+        for resolution in iconResolutions
+        {
+            let targetIconPath = outputDir.stringByAppendingPathComponent("icon-\(resolution).png")
+            NSFileManager.defaultManager().copyItemAtPath(iconPath, toPath: targetIconPath, error:nil)
+            
+            scaleIcon(targetIconPath, resolution)
+        }
+    }
+}
+
+extension String {
+    // string[i] -> one string char
+    subscript(pos: Int) -> String { return String(Array(self)[min(self.length-1,max(0,pos))]) }
+    var length: Int { return countElements(self) }
+}
+
+// =======
+
+if Process.arguments.count > 1
+{
+    var inputFilenames = Process.arguments
+    inputFilenames.removeAtIndex(0)
+    
+    var filePathsArr = Array<String>()
+    
+    for filename in inputFilenames
+    {
+        if filename[0] == "/" {
+            filePathsArr.append(filename)
+            continue
+        }
+        
+        if filename[0] == "~" {
+            filePathsArr.append(filename.stringByExpandingTildeInPath)
+            continue
+        }
+        
+        // if we come here, filename should be relative path
+        let fileUrl = NSURL(string: filename, relativeToURL: NSURL(fileURLWithPath: currentPath))
+        if let u = fileUrl {
+            if let p = u.path {
+                filePathsArr.append(p)
+            }
+        }
+    }
+    
+    createIconsForFilePaths(filePathsArr)
 }
 else
 {
-    if NSFileManager.defaultManager().fileExistsAtPath(outputDir) {
-        NSFileManager.defaultManager().removeItemAtPath(outputDir, error: nil)
-    }
-    
-    NSFileManager.defaultManager().createDirectoryAtPath(outputDir, withIntermediateDirectories: false, attributes: nil, error: nil)
-    
-    for resolution in iconResolutions
-    {
-        let targetIconPath = outputDir.stringByAppendingPathComponent("icon-\(resolution).png")
-        NSFileManager.defaultManager().copyItemAtPath(sourceIcon, toPath: targetIconPath, error:nil)
-        
-        scaleIcon(targetIconPath, resolution)
-    }
+    createIconsForFilePaths([defaultSourceIconPath])
 }
