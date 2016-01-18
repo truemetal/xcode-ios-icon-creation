c7f22b891b0f5fa4d9a29a16b90e0343dee65d07
True Metal <true.metal.of.steel@gmail.com>
2016-01-18 18:35:12 +0200

    fixed error checking + minor fixes

--- make_icons.swift
+++ make_icons.swift
@@ -5,12 +5,13 @@ let currentPath = NSFileManager.defaultManager().currentDirectoryPath
 let defaultSourceIconPath = NSURL.fileURLWithPath(currentPath).URLByAppendingPathComponent("icon.png")
 let iconResolutions = [29, 40, 48, 50, 55, 57, 58, 60, 72, 76, 80, 87, 88, 100, 114, 120, 144, 152, 167, 171, 172, 180, 196]
 
+extension String { subscript(pos: Int) -> String { return String(characters[startIndex.advancedBy(0)]) } }
+
 func outputDirPathForIconPath(iconPath: NSURL) -> NSURL
 {
-    let a = iconPath.URLByDeletingPathExtension?.lastPathComponent
-    let b = iconPath.URLByDeletingLastPathComponent
-    let c = b!.URLByAppendingPathComponent("\(a!)_output")
-    return c
+    let iconName = iconPath.URLByDeletingPathExtension?.lastPathComponent
+    let parentPath = iconPath.URLByDeletingLastPathComponent
+    return parentPath!.URLByAppendingPathComponent("\(iconName!)_output")
 }
 
 func scaleIcon(iconPath: NSURL, resolution: Int)
@@ -19,6 +20,7 @@ func scaleIcon(iconPath: NSURL, resolution: Int)
     task.launchPath = "/usr/bin/sips"
     task.arguments = ["-Z", "\(resolution)", iconPath.path!]
     task.launch()
+    task.waitUntilExit()
 }
 
 func createIconsForFilePaths(filePaths : Array<NSURL>)
@@ -31,7 +33,7 @@ func createIconsForFilePaths(filePaths : Array<NSURL>)
             print("Issue: file does not exist \(filePath)")
         }
         
-        if NSFileManager.defaultManager().fileExistsAtPath(outputDirPathForIconPath(filePath).absoluteString) {
+        if NSFileManager.defaultManager().fileExistsAtPath(outputDirPathForIconPath(filePath).path!) {
             failedInputsCheck = true
             print("Issue: this directory exists and will be re-written \(outputDirPathForIconPath(filePath))")
         }
@@ -39,35 +41,31 @@ func createIconsForFilePaths(filePaths : Array<NSURL>)
     
     if failedInputsCheck { return }
     
-    for iconPath in filePaths
-    {
-        print("Processing file: \(iconPath)")
-        
-        let outputDir = outputDirPathForIconPath(iconPath)
-        
-        if !NSFileManager.defaultManager().fileExistsAtPath(outputDir.absoluteString) {
-            
-            do {
-                try NSFileManager.defaultManager().createDirectoryAtPath(outputDir.path!, withIntermediateDirectories: false, attributes: [:])
-            } catch {
-                print("Error creating image folder")
-            }
-        }
-        
-        
-        for resolution in iconResolutions
-        {
-            let targetIconPath = outputDir.URLByAppendingPathComponent("icon-\(resolution).png")
-            do {
-                try NSFileManager.defaultManager().copyItemAtPath(iconPath.path!, toPath: targetIconPath.path!)
-            } catch {
-                print("Error copying images")
-            }
-            
-            
-            scaleIcon(targetIconPath, resolution: resolution)
-        }
-    }
+	for iconPath in filePaths
+	{
+		print("Processing file: \(iconPath)")
+	
+		let outputDir = outputDirPathForIconPath(iconPath)
+	
+		do {
+			try NSFileManager.defaultManager().createDirectoryAtPath(outputDir.path!, withIntermediateDirectories: false, attributes: nil)
+		} catch {
+			print("Error creating image folder")
+		}
+	
+	
+		for resolution in iconResolutions
+		{
+			let targetIconPath = outputDir.URLByAppendingPathComponent("icon-\(resolution).png")
+			do {
+				try NSFileManager.defaultManager().copyItemAtPath(iconPath.path!, toPath: targetIconPath.path!)
+			} catch {
+				print("Error copying images")
+			}
+		
+			scaleIcon(targetIconPath, resolution: resolution)
+		}
+	}
 }
 
 // =======
@@ -81,12 +79,12 @@ if Process.arguments.count > 1
     
     for filename in inputFilenames
     {
-        if filename.characters[filename.startIndex.advancedBy(0)]  == "/" {
+        if filename[0]  == "/" {
             filePathsArr.append(NSURL.fileURLWithPath(filename))
             continue
         }
         
-        if filename.characters[filename.startIndex.advancedBy(0)]  == "~" {
+        if filename[0]  == "~" {
             filePathsArr.append(NSURL.fileURLWithPath(filename))
             continue
         }
