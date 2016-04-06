1a05f54bc9d7ba6df232f974723a3ae64ddc5497
True Metal <true.metal.of.steel@gmail.com>
2016-04-06 14:04:22 +0300

    Improves naming of generated icons; implements icons for iphone/ipad iOS 7+, and all of iwatch

--- make_icons.swift
+++ make_icons.swift
@@ -3,7 +3,9 @@ import Foundation
 
 let currentPath = NSFileManager.defaultManager().currentDirectoryPath
 let defaultSourceIconPath = NSURL.fileURLWithPath(currentPath).URLByAppendingPathComponent("icon.png")
-let iconResolutions = [29, 40, 48, 50, 55, 57, 58, 60, 72, 76, 80, 87, 88, 100, 114, 120, 144, 152, 167, 171, 172, 180, 196]
+let iphone7AndLaterResolutions = [("iphone-29@2x", 58), ("iphone-29@3x", 87), ("iphone-40@2x", 80), ("iphone-40@3x", 120), ("iphone-60@2x", 120), ("iphone-60@3x", 180)]
+let ipad7AndLaterResolutions = [("ipad-29@1x", 29), ("ipad-29@2x", 58), ("ipad-40@1x", 40), ("ipad-40@2x", 80), ("ipad-76@1x", 76), ("ipad-76@2x", 152), ("ipad-83.5@2x", 167)]
+let iwatchResolutions = [("iwatch-24@2x", 48), ("iwatch-27.5@2x", 55), ("iwatch-29@2x", 58), ("iwatch-29@3x", 87), ("iwatch-40_all@2x", 80), ("iwatch-44@2x", 88), ("iwatch-86@2x", 172), ("iwatch-98@1x", 196)]
 
 extension String {
     subscript(pos: Int) -> String { return String(characters[startIndex.advancedBy(0)]) }
@@ -43,31 +45,37 @@ func createIconsForFilePaths(filePaths : Array<NSURL>)
     
     if failedInputsCheck { return }
     
-	for iconPath in filePaths
-	{
-		print("Processing file: \(iconPath)")
-	
-		let outputDir = outputDirPathForIconPath(iconPath)
-	
-		do {
-			try NSFileManager.defaultManager().createDirectoryAtPath(outputDir.path!, withIntermediateDirectories: false, attributes: nil)
-		} catch {
-			print("Error creating image folder")
-		}
-	
-	
-		for resolution in iconResolutions
-		{
-			let targetIconPath = outputDir.URLByAppendingPathComponent("icon-\(resolution).png")
-			do {
-				try NSFileManager.defaultManager().copyItemAtPath(iconPath.path!, toPath: targetIconPath.path!)
-			} catch {
-				print("Error copying images")
-			}
-		
-			scaleIcon(targetIconPath, resolution: resolution)
-		}
-	}
+    for iconPath in filePaths
+    {
+        print("Processing file: \(iconPath)")
+        
+        let outputDir = outputDirPathForIconPath(iconPath)
+        
+        do {
+            try NSFileManager.defaultManager().createDirectoryAtPath(outputDir.path!, withIntermediateDirectories: false, attributes: nil)
+        } catch {
+            print("Error creating image folder")
+        }
+        
+        makeIconsForResolutionsArr(iphone7AndLaterResolutions, iconPath:iconPath, outputDir:outputDir)
+        makeIconsForResolutionsArr(ipad7AndLaterResolutions, iconPath:iconPath, outputDir:outputDir)
+        makeIconsForResolutionsArr(iwatchResolutions, iconPath:iconPath, outputDir:outputDir)
+    }
+}
+
+func makeIconsForResolutionsArr(resolutionsArr:[(String,Int)], iconPath:NSURL, outputDir:NSURL)
+{
+    for (prefix, resolution) in resolutionsArr
+    {
+        let targetIconPath = outputDir.URLByAppendingPathComponent("icon-\(prefix).png")
+        do {
+            try NSFileManager.defaultManager().copyItemAtPath(iconPath.path!, toPath: targetIconPath.path!)
+        } catch {
+            print("Error copying images")
+        }
+        
+        scaleIcon(targetIconPath, resolution: resolution)
+    }
 }
 
 // =======
